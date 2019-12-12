ad2c8309cebbc50161405471682d6f0c03ba4d1b
Vaishnav S Menon <vaishnavsmn@gmail.com>
2019-12-12 20:03:30 +0530

    Core Functionality - Filling

---
+++ core-js/core.js
@@ -0,0 +1,107 @@
+async function get_template_element(url){
+    let response = await fetch(url);
+    let html = await response.text();
+    var elem = document.createElement("object");
+    elem.innerHTML = html;
+    console.log(html);
+    return elem;
+    // .then((response)=> response.text())
+    // .then((html)=>{
+    //     var elem = document.createElement("object");
+    //     elem.innerHTML = html;
+    //     return elem;
+    // })
+    // .catch((error) => {
+    //     console.warn(error);
+    //     alert("There was an error in loading "+url);
+    //     return undefined;
+    // });
+}
+
+async function fill_template_with_data(data, url){
+    if(!(data instanceof Array)) data = [data];
+    let orig_doc = await get_template_element(url);
+    if(orig_doc == undefined) return undefined;
+    var result = [];
+    for(let i=0; i<data.length; ++i){
+        let current_elem = orig_doc.cloneNode(true);
+        fill_with_data(data[i], current_elem)
+        result.push(current_elem);
+    }
+    return result;
+}
+
+function fill_with_data(data, doc){
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            Array.from(doc.getElementsByClassName("data-box")).filter((e)=>{return e.getAttribute('x-name') == key;}).forEach((e)=>{
+                e.innerText = data[key];
+            })
+            Array.from(doc.getElementsByClassName("data-table")).filter((e)=>{return e.getAttribute('x-name') == key;}).forEach((e)=>{
+                var num_elements = 0;
+                var num_rows = 0;
+                var columns = Array.from(e.getElementsByTagName('th')).map((e)=>{return e.getAttribute('x-name')})
+                for (var iii = 0; iii<columns.length; ++iii) {
+                    let table_item = columns[iii];
+                    if (data[key].hasOwnProperty(table_item)) {
+                        console.log("Hi "+table_item);
+                        while(data[key][table_item].length > num_rows){
+                            let row = document.createElement("tr");
+                            for(let i=0;i<num_elements;++i) row.appendChild(document.createElement('td'));
+                            e.appendChild(row);
+                            ++num_rows;
+                        }
+                        var rows = Array.from(e.getElementsByTagName('tr')).splice(1);
+                        for(let i=0; i<rows.length; ++i){
+                            let row = document.createElement('td');
+                            row.innerText = data[key][table_item][i] || '';
+                            rows[i].appendChild(row);
+                        }
+                        num_elements++;
+                    }
+                    else {
+                        var rows = Array.from(e.getElementsByTagName('tr')).splice(1);
+                        for(let i=0; i<rows.length; ++i){
+                            let row = document.createElement('td');
+                            rows[i].appendChild(row);
+                        }
+                        num_elements++;
+                    }
+                }
+            })
+        }
+    }
+}
+async function main(){
+    var final_result = await fill_template_with_data([
+        {
+            "name": "This is Name", 
+            "age": 20, 
+            "phone_num":"9845666432", 
+            "marks":32.333, 
+            'key-value-pair':{
+                'key':['k1','k2','jb','k3'],
+                'value':['value 1','value 2','test3']
+            }
+        },
+        {
+            "name": "Name 2", 
+            "age": 27, 
+            "phone_num":"2223335321", 
+            "marks":0.2
+        },
+        {
+            "name": "Another Name", 
+            "age": 20, 
+            'key-value-pair':{
+                'value':['hey look', "it's a key!"]
+            }
+        },
+    ],'http://127.0.0.1:8000/media/templates/TestTemp_0.html');
+    console.log(final_result)
+    for(var i=0; i<final_result.length; ++i){
+        document.getElementById('body').appendChild(final_result[i]);
+        document.getElementById('body').appendChild(document.createElement("hr"));
+    }
+}
+(()=>{main()})();
