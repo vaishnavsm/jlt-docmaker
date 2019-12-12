3ed3436e7669e7bbd887d4c66ab6401e751c28a5
Vaishnav S Menon <vaishnavsmn@gmail.com>
2019-12-12 23:00:25 +0530

    Export to PDF

--- core-js/core.js
+++ core-js/core.js
@@ -3,19 +3,7 @@ async function get_template_element(url){
     let html = await response.text();
     var elem = document.createElement("object");
     elem.innerHTML = html;
-    console.log(html);
     return elem;
-    // .then((response)=> response.text())
-    // .then((html)=>{
-    //     var elem = document.createElement("object");
-    //     elem.innerHTML = html;
-    //     return elem;
-    // })
-    // .catch((error) => {
-    //     console.warn(error);
-    //     alert("There was an error in loading "+url);
-    //     return undefined;
-    // });
 }
 
 async function fill_template_with_data(data, url){
@@ -44,7 +32,6 @@ function fill_with_data(data, doc){
                 for (var iii = 0; iii<columns.length; ++iii) {
                     let table_item = columns[iii];
                     if (data[key].hasOwnProperty(table_item)) {
-                        console.log("Hi "+table_item);
                         while(data[key][table_item].length > num_rows){
                             let row = document.createElement("tr");
                             for(let i=0;i<num_elements;++i) row.appendChild(document.createElement('td'));
@@ -84,24 +71,40 @@ async function main(){
                 'value':['value 1','value 2','test3']
             }
         },
-        {
-            "name": "Name 2", 
-            "age": 27, 
-            "phone_num":"2223335321", 
-            "marks":0.2
-        },
-        {
-            "name": "Another Name", 
-            "age": 20, 
-            'key-value-pair':{
-                'value':['hey look', "it's a key!"]
-            }
-        },
+        // {
+        //     "name": "Name 2", 
+        //     "age": 27, 
+        //     "phone_num":"2223335321", 
+        //     "marks":0.2
+        // },
+        // {
+        //     "name": "Another Name", 
+        //     "age": 20, 
+        //     'key-value-pair':{
+        //         'value':['hey look', "it's a key!"]
+        //     }
+        // },
     ],'http://127.0.0.1:8000/media/templates/TestTemp_0.html');
-    console.log(final_result)
     for(var i=0; i<final_result.length; ++i){
-        document.getElementById('body').appendChild(final_result[i]);
-        document.getElementById('body').appendChild(document.createElement("hr"));
+        // Image/Canvas Output
+        // document.getElementById('body').appendChild(final_result[i]);
+        // var doc_fixed = await html2canvas(final_result[i], {logging: false});
+        // document.getElementById("body").removeChild(final_result[i]);
+
+        // document.getElementById('body').appendChild(doc_fixed);
+        // document.getElementById('body').appendChild(document.createElement("hr"));
+
+        // var img=doc_fixed.toDataURL("image/png");
+        // var doc = new jsPDF('p', 'pt', 'letter');
+        // doc.addImage(img,'JPEG',20,20);
+        // // doc.save('test.pdf');
+        // doc.output("dataurlnewwindow");
+
+        // Text Output:
+        var doc = new jsPDF('p', 'pt', 'letter');
+        doc.fromHTML(final_result[i]);
+        doc.output("dataurlnewwindow");
+        
     }
 }
 (()=>{main()})();
